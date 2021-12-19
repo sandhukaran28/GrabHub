8736676a77200472f35c8036492512a04374d574
sandhukaran28 <60167401+sandhukaran28@users.noreply.github.com>
2021-12-19 12:15:27 +0530

    Login UI Updated

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,46 +1,66 @@
-    import React,{useContext} from 'react'
-    import styles from './Register.module.css'
-    import { useHistory } from 'react-router'
-    import axios from 'axios';
-    import AuthContext from '../../store/Auth-context';
-
-    const Login = () => {
-
-        const history = useHistory();
-        const cartCtx = useContext(AuthContext);        
-    const UserformSubmit = async(e)=>{
-
+import React, { useContext } from "react";
+import styles from "./Register.module.css";
+import { useHistory } from "react-router";
+import axios from "axios";
+import svg from "../../assets/login.svg";
+import AuthContext from "../../store/Auth-context";
+const Login = () => {
+  const history = useHistory();
+  const cartCtx = useContext(AuthContext);
+  const UserformSubmit = async (e) => {
     e.preventDefault();
-    const  email = e.target.elements.email.value;
+    const email = e.target.elements.email.value;
     const pass = e.target.elements.pass.value;
-    const data ={
-        email:email,
-        password:pass,
-    }
-    await axios.post('http://localhost:8000/auth/login',data)
-    .then(()=>{
+    const data = {
+      email: email,
+      password: pass,
+    };
+    await axios
+      .post("http://localhost:8000/auth/login", data)
+      .then(() => {
         console.log("done");
-    })
-    .catch((e)=>{
+      })
+      .catch((e) => {
         console.log(e);
-    })
+      });
     await cartCtx.getLoggedIn();
-    history.push('/');
-    }
+    history.push("/");
+  };
 
-        return (
-            <div id={styles.register}>
-            <h2>Log In</h2>
-            <form onSubmit={UserformSubmit} >
-                <label htmlFor="email">Email</label>
-                <input  type="email" name="email" id="email"  placeholder="Email"/>
-                <label htmlFor="pass">Password</label>
-                <input  type="password" name="pass" id="pass" placeholder="Password" />
-                <button type="submit">Log In</button>
-            </form>
-            
+  return (
+    <div id={styles.outer}>
+      <div className={styles.left}>
+        <img src={svg} alt="login" />
+      </div>
+      <div className={styles.right}>
+        <div className={styles.rDiv}>
+          <h2>Log In to GrabHub</h2>
+          <hr />
+          <form onSubmit={UserformSubmit}>
+            <div className={styles.group}>
+              <label htmlFor="email">Email Address</label>
+              <input
+                className={styles.authInput}
+                type="email"
+                name="email"
+                id="email"
+              />
+            </div>
+            <div className={styles.group}>
+              <label htmlFor="pass">Password</label>
+              <input
+                className={styles.authInput}
+                type="password"
+                name="pass"
+                id="pass"
+              />
+            </div>
+            <button type="submit">Log In</button>
+          </form>
         </div>
-        )
-    }
+      </div>
+    </div>
+  );
+};
 
-    export default Login
+export default Login;
--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -1,55 +1,57 @@
-import axios from 'axios';
-import React,{useContext} from 'react'
-import { useHistory } from 'react-router';
-import AuthContext from '../../store/Auth-context';
-import styles from './Register.module.css'
+import axios from "axios";
+import React, { useContext } from "react";
+import { useHistory } from "react-router";
+import AuthContext from "../../store/Auth-context";
+import styles from "./Register.module.css";
 const Register = () => {
+  const history = useHistory();
+  const cartCtx = useContext(AuthContext);
+  const UserformSubmit = async (e) => {
+    e.preventDefault();
+    console.log("here");
+    const name = e.target.elements.name.value;
+    const email = e.target.elements.email.value;
+    const pass = e.target.elements.pass.value;
+    const verify = e.target.elements.confirm.value;
+    const data = {
+      name: name,
+      email: email,
+      password: pass,
+      passwordVerify: verify,
+    };
+    await axios
+      .post("http://localhost:8000/auth/new", data)
+      .then(() => {
+        console.log("done");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    await cartCtx.getLoggedIn();
+    history.push("/");
+  };
 
-    const history = useHistory();
-    const cartCtx = useContext(AuthContext);   
-    const UserformSubmit = async(e)=>{
+  return (
+    <div id={styles.register}>
+      <h2>Register a New Account</h2>
+      <form onSubmit={UserformSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" placeholder="Email" />
+        <label htmlFor="pass">Password</label>
+        <input type="password" name="pass" id="pass" placeholder="Password" />
+        <label htmlFor="verify">Confirm Your Password</label>
+        <input
+          type="password"
+          name="confirm"
+          id="confirm"
+          placeholder="Confirm Your Password"
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
 
-        e.preventDefault();
-        console.log("here");
-        const name = e.target.elements.name.value;
-        const  email = e.target.elements.email.value;
-        const pass = e.target.elements.pass.value;
-        const verify = e.target.elements.confirm.value;
-        const data ={
-            name:name,
-            email:email,
-            password:pass,
-            passwordVerify:verify
-
-        }
-        await axios.post('http://localhost:8000/auth/new',data)
-        .then(()=>{
-            console.log("done");
-        })
-        .catch((e)=>{
-            console.log(e);
-        })
-        await cartCtx.getLoggedIn();
-    history.push('/');
-    }
-
-    return (
-        <div id={styles.register}>
-            <h2>Register a New Account</h2>
-            <form onSubmit={UserformSubmit} >
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
-                <label htmlFor="email">Email</label>
-                <input  type="email" name="email" id="email"  placeholder="Email"/>
-                <label htmlFor="pass">Password</label>
-                <input  type="password" name="pass" id="pass" placeholder="Password" />
-                <label htmlFor="verify">Confirm Your Password</label>
-                <input type="password" name="confirm" id="confirm" placeholder="Confirm Your Password" />
-                <button type="submit">Register</button>
-            </form>
-           
-        </div>
-    )
-}
-
-export default Register
+export default Register;
